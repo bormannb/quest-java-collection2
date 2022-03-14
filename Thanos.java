43135927942fd4a488967a5249b700c4bab57991
bormannb <99260828+bormannb@users.noreply.github.com>
2022-03-14 08:57:42 +0100

    quest-java-collection2

--- Thanos.java
+++ Thanos.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.catalog.Catalog;
+
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Thanos {
 
     public static void main(String[] args) {
@@ -15,12 +20,31 @@ public class Thanos {
 
 
         System.out.println("\nOrder by name:");
+        Collections.sort(heroes);
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
 
 
         System.out.println("\nOrder by age:");
+
+
+        Comparator<Hero> heroAgeComparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero t1,Hero t2 ) { 
+             
+                if(t1.getAge() < t2.getAge()){
+                    return 1;
+                } else if(t1.getAge()>t2.getAge()){
+                    return -1;  
+                } else {
+                    return 0; 
+                }  
+            }
+        };
+       
+        Collections.sort(heroes, heroAgeComparator);
+        
         showList(heroes);
     }
 
@@ -29,4 +53,5 @@ public class Thanos {
             System.out.println(heroe.getName() + ", " + heroe.getAge());
         }
     }
+
 }
